7a2b46a6d75c413e0bd3ff004fdfca9679726eab
Théodore Pietron <77795072+TheodorePietron@users.noreply.github.com>
2022-03-25 12:28:09 +0100

    Add files via upload

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -6,7 +6,6 @@ use App\Repository\ProduitRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ProduitRepository::class)
@@ -22,85 +21,29 @@ class Produit
 
     /**
      * @ORM\Column(type="string", length=40)
-     * @Assert\NotBlank
-     * @Assert\Length(
-     *     min = 3,
-     *     max = 40,
-     *     minMessage = "Libellé trop court",
-     *     maxMessage = "Libellé trop long"
-     * )
-
      */
     private $libelle;
 
     /**
      * @ORM\Column(type="decimal", precision=7, scale=2)
-     * @Assert\NotBlank(message = "Prix obligatoire")
-     * @Assert\Range(min = 0.1, max = 999)
-
      */
     private $prix;
 
     /**
      * @ORM\Column(type="datetime")
-     * @Assert\Type("\DateTime")
      */
     private $dateCreation;
 
-       /**
+    /**
      * @ORM\ManyToOne(targetEntity=Categorie::class, inversedBy="produits")
      * @ORM\JoinColumn(nullable=false)
      */
     private $categorie;
 
-
     /**
      * @ORM\ManyToMany(targetEntity="App\Entity\Recette", mappedBy="produits")
      */
     private $recettes;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     * @Assert\NotBlank(message = "Description obligatoire")
-     * @Assert\Length(
-     *     min = 15,
-     *     max = 255,
-     *     minMessage = "Description trop courte",
-     *     maxMessage = "Description trop longue"
-     * )
-
-     */
-    private $description;
-
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $cru;
-
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $cuit;
-
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $bio;
-
-    /**
-     * @ORM\Column(type="date", nullable=true)
-     * @Assert\Type("\DateTime")
-     */
-    private $debutDisponibilite;
-
-    /**
-     * @ORM\Column(type="date", nullable=true)
-     * @Assert\Type("\DateTime")
-     * @Assert\Range(minPropertyPath="debutDisponibilite")
-     */
-    private $finDisponibilite;
-
-
 
     public function getId(): ?int
     {
@@ -136,10 +79,6 @@ class Produit
         return $this->dateCreation;
     }
 
-    public function __toString(){
-        return $this->getDateCreation();
-    }
-
     public function setDateCreation(\DateTimeInterface $dateCreation): self
     {
         $this->dateCreation = $dateCreation;
@@ -150,7 +89,7 @@ class Produit
     public function __construct()
     {
         $this->dateCreation = new \DateTime('now');
-        $this->recette = new ArrayCollection();
+        $this->recettes = new ArrayCollection();
     }
 
     public function getCategorie(): ?Categorie
@@ -165,10 +104,10 @@ class Produit
         return $this;
     }
 
-      /**
-     * @return Collection|Recette[]|null
+    /**
+     * @return Collection|Recette[]
      */
-    public function getRecettes(): ?Collection
+    public function getRecettes(): Collection
     {
         return $this->recettes;
     }
@@ -192,78 +131,4 @@ class Produit
 
         return $this;
     }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(?string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    public function getCru(): ?bool
-    {
-        return $this->cru;
-    }
-
-    public function setCru(bool $cru): self
-    {
-        $this->cru = $cru;
-
-        return $this;
-    }
-
-    public function getCuit(): ?bool
-    {
-        return $this->cuit;
-    }
-
-    public function setCuit(bool $cuit): self
-    {
-        $this->cuit = $cuit;
-
-        return $this;
-    }
-
-    public function getBio(): ?bool
-    {
-        return $this->bio;
-    }
-
-    public function setBio(bool $bio): self
-    {
-        $this->bio = $bio;
-
-        return $this;
-    }
-
-    public function getDebutDisponibilite(): ?\DateTimeInterface
-    {
-        return $this->debutDisponibilite;
-    }
-
-    public function setDebutDisponibilite(?\DateTimeInterface $debutDisponibilite): self
-    {
-        $this->debutDisponibilite = $debutDisponibilite;
-
-        return $this;
-    }
-
-    public function getFinDisponibilite(): ?\DateTimeInterface
-    {
-        return $this->finDisponibilite;
-    }
-
-    public function setFinDisponibilite(?\DateTimeInterface $finDisponibilite): self
-    {
-        $this->finDisponibilite = $finDisponibilite;
-
-        return $this;
-    }
-
-
 }
