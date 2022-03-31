dbb04aec5196f3afa999519048cbcaadaa0d7fee
TristanLhotellier <77794932+TristanLhotellier@users.noreply.github.com>
2022-03-31 17:11:14 +0200

    api

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,6 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Component\Validator\Constraints\DateTimeZone;
+
 
 class AccueilController extends AbstractController
 {
@@ -13,8 +18,30 @@ class AccueilController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
-        ]);
+
+
+                // récupérer la variable d'environnement désignant l'URl de l'API
+                $urlAPI = $_SERVER['URL_API_OWM'];
+
+                // Initialiser une session CURL
+                $clientURL = curl_init();
+                // Récupérer le contenu de la page
+                curl_setopt($clientURL, CURLOPT_RETURNTRANSFER, 1);
+                // Transmettre l'URL
+                curl_setopt($clientURL, CURLOPT_URL, $urlAPI);
+                // Exécutez la requête HTTP
+                $reponse = curl_exec($clientURL);
+                // Fermer la session
+                curl_close($clientURL);
+                // Récupérer les données au format JSON
+                $donneesTemps = json_decode($reponse);
+
+                // $dateJour = new DateTime('now', new DateTimeZone('Europe/Paris'));
+
+                //   A COMPLETER
+                return $this->render('accueil/index.html.twig', [
+                        'donneesTemps' => $donneesTemps,
+                        // 'dateJour'=>$dateJour,
+                ]);
     }
 }
